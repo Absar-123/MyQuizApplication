78010ec138bd51c20acb189b7dfcb1210acbc63e
Absar-123 <absarmd7488@gmail.com>
2023-06-11 23:06:50 +0530

    initial_commit

---
+++ app/src/main/java/com/absar/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,83 @@
+package com.absar.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+
+            "1. Javascript is an _______ language",
+            "2. Which one of the following also known as Conditional Expression:",
+            "3. The function and  var are known as:",
+            "4. Which of the following variables takes precedence over the others if the names are the same?",
+            "5. Which one of the following is the correct way for calling the JavaScript code?",
+            "6. Which of the following type of a variable is volatile?",
+            "7. Which of the following number object function returns the value of the number?",
+            "8. In JavaScript, what will be used for calling the function definition expression:",
+            "9. Which one of the following statement is most suitable to check if the pattern matches with the sting \"text\".",
+            "10. Which one of the following is used for the calling a function or a method in the JavaScript:",
+            "11. What are the three important manipulations for a loop on a loop variable?",
+            "12. Which one of the following is not considered as \"statement\" in the JavaScript?",
+            "13. What if we define a \"for\" loop and it removes one of the properties that has not yet been enumerated?",
+            "14. What is the basic purpose of the \"toLocateString()\" method?",
+            "15. A set of unordered properties that, has a name and value is called______",
+            "16. A collection of elements of the same data type which may either in order or not, is called _____.",
+            "17.Every object contains three object attributes that are _______.",
+            "18. The linkage of a set of prototype objects is known as______",
+
+    };
+
+    public static String choices[][] = {
+
+            {"Object-Oriented", "Object-Based", "Assembly-language", "High-level"},
+            {"Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"},
+            {"Keywords", "Data types", "Declaration statements", "Prototypes"},
+            {"Global variable", "The local element", "The two of the above", "None of the above"},
+            {"Preprocessor", "Triggering Event", "RMI", "Function/Method"},
+            {"Mutable variable", "Dynamic variable", "Volatile variable", "Immutable variable"},
+            {"toString()", "valueOf()", "toLocaleString()", "toPrecision()"},
+            {"Function prototype", "Function literal", "Function calling", "Function declaration"},
+            {"test(text)", "equals(pattern)", "test(pattern)", "text==pattern"},
+            {"Property Access", "Expression Functional expression", "Invocation expression", "Primary expression"},
+            {"Updation, Incrementation, Initialization", "Initialization, Testing, Incrementation", "Testing, Updation, Testing", "Initialization, Testing, Updation"},
+            {"use strict", "debugger", "if", "with"},
+            {"The removed property will be stored in a cache", "The loop will not run at all", "That property will be enumerated", "That specific property will not be enumerated"},
+            {"It returns a localised object representation", "It returns a localized string representation of the object", "It return a local time in the string format", "It return a parsed string"},
+            {"String", "Array", "Serialized Object", "Object"},
+            {"String", "Array", "Serialized Object", "Object"},
+            {"Prototype, class, object's extensible flag", "Prototype, class, objects' parameters", "Class, parameters, object's extensible flag", "Native object, Classes and Interfaces and Object's extensible flag"},
+            {"prototype stack", "prototype", "prototype class", "prototype chain"},
+
+
+    };
+
+    public static String correctAnswers[] ={
+
+            "Object-Oriented",
+            "immediate if",
+            "Declaration statements",
+            "The local element",
+            "Function/Method",
+            "Mutable variable",
+            "valueOf()",
+            "Function literal",
+            "text==pattern",
+            "Invocation expression",
+            "Initialization, Testing, Updation",
+            "use strict",
+            "That specific property will not be enumerated",
+            "It returns a localized string representation of the object",
+            "Object",
+            "Array",
+            "Prototype, class, object's extensible flag",
+            "prototype chain",
+
+
+
+
+
+    };
+
+
+
+
+
+}
